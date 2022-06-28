a85b432118de3adccf095ae55393521020cd0c79
iammac2 <ian_mcgraw@yahoo.co.uk>
2022-06-28 12:07:01 +0000

    Fixes for URL and setting chome driver

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,18 +2,16 @@ package pages;
 
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
-import net.thucydides.core.annotations.DefaultUrl;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.WhenPageOpens;
 
-@DefaultUrl("https://www.saucedemo.com/")
 public class LoginPage extends PageObject {
 
 	@FindBy(id = "login-button")
 	private WebElementFacade loginButton;
 
 	public void navigateToLogin() {
-		open();
+		getDriver().get("https://www.saucedemo.com");
 	}
 
 	@WhenPageOpens
